5336bb9653a8bba145257bcdd0daf61f3087d075
Corin Langosch <info@netskin.com>
2010-09-21 22:43:13 +0100

    fix wrong sql condition

--- lib/i18n_backend_sequel/translation.rb
+++ lib/i18n_backend_sequel/translation.rb
@@ -67,7 +67,7 @@ module I18n
           end
 
           namespace = "#{keys.last}#{I18n::Backend::Flatten::FLATTEN_SEPARATOR}%"
-          filter(:key => keys).or(:key.like(namespace))
+          filter{({:key => keys}) | (key.like(namespace))}
         end
 
         plugin :serialization, :json, :value, :interpolations
